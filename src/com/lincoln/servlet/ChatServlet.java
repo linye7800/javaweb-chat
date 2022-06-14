906f51c549e218e4ac21f8659e59f06125dcb44c
linye <linye102@163.com>
2022-06-14 22:58:25 +0800

    0530 finsh

--- src/com/lincoln/servlet/ChatServlet.java
+++ src/com/lincoln/servlet/ChatServlet.java
@@ -1,13 +1,9 @@
 package com.lincoln.servlet;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * @author linye
